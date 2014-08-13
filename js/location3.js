47a82b55f71e9a19bda066ed37dcf1198f63aa86
taegeunjang <taegeunjang@gmail.com>
2014-08-13 18:36:24 +0900

    modify some code

--- js/location3.js
+++ js/location3.js
@@ -1,7 +1,9 @@
-$('#btn').click(function(){
-  var chk=$('.check:eq(2)').is(":checked");
-  if(chk)
-    alert('성공 :  통과');
-  else
-    alert('실패 : 3번을 선택해주세요');
+$(function(){
+  $('#btn').click(function(){
+    var chk=$('.check:eq(2)').is(":checked");
+    if(chk)
+      alert('성공 :  통과');
+    else
+      alert('실패 : 3번을 선택해주세요');
+  });
 });
--- js/location4.js
+++ js/location4.js
@@ -1,6 +1,24 @@
 var len=$('.image-class').length;
-$('#btn').click(function(){
-  var index=$('.image-class:last').index();
-  index=((index+1)%len)+1;
-  $('#imgArea').append('<img class="image-class img-rounded" src="img/'+index+'.png" /> ');
+var counter = 1;
+$(document).ready(function(){
+  $('#btn').click(function(){
+    var img = $("<img>");
+    img.addClass("image-class");
+    img.addClass("img-rounded");
+    img.attr("src", "img/"+counter+".png");
+    img.click(function() {
+      var n=$(this).index();
+      $('.image-class').eq(n).remove();
+    });
+
+    $('#imgArea').append(img);
+    counter++;
+    if(counter > 5) counter = 1;
+  });
+
+  // 이미지 삭제하기
+  $('.image-class').click(function() {
+    var n=$(this).index();
+    $('.image-class').eq(n).remove();
+  });
 });
